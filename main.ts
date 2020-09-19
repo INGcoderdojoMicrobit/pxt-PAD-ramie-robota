f973c42fd449f2ed8bfe2297bd8158f826884087
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-09-19 13:55:04 +0200

    pierwsza wersja - czytanie z PIN0- i PIN1 stanu klawiszy

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
+let item = 0
+I2C_LCD1602.LcdInit(63)
+basic.showIcon(IconNames.Heart)
+basic.forever(() => {
+basic.showIcon(IconNames.Angry)
+    item += 1
+    I2C_LCD1602.BacklightOff()
+    I2C_LCD1602.ShowNumber(item, 0, 1)
+    basic.pause(100)
+    I2C_LCD1602.BacklightOn()
+    basic.showIcon(IconNames.Cow)
+    basic.pause(100)
+    I2C_LCD1602.clear()
+    I2C_LCD1602.ShowNumber(pins.analogReadPin(AnalogPin.P0),10, 1)
+    I2C_LCD1602.ShowNumber(pins.analogReadPin(AnalogPin.P1),10, 0)
 
+    music.playTone(Note.C, 10)
+})
