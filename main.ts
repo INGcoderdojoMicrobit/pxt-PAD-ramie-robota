41691235d947f13d454c5e8b8bcc0faad6a8a54e
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-10 10:26:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,8 +15,6 @@ basic.forever(function () {
     // basic.showIcon(IconNames.Cow)
     basic.pause(100)
     I2C_LCD1602.clear()
-    I2C_LCD1602.ShowNumber(pins.analogReadPin(AnalogPin.P1), 10, 1)
-    I2C_LCD1602.ShowNumber(pins.analogReadPin(AnalogPin.P2), 10, 0)
     arp1 = pins.analogReadPin(AnalogPin.P1)
     arp2 = pins.analogReadPin(AnalogPin.P2)
     if (arp1 >= 340 && arp1 <= 370) {
@@ -27,22 +25,22 @@ basic.forever(function () {
     } else if (arp1 >= 20 && arp1 <= 50) {
         // UP
         led.plot(1, 0)
-        I2C_LCD1602.ShowString("U", 1, 0)
+        I2C_LCD1602.ShowString("gora", 1, 0)
         radio.sendString("U")
     } else if (arp1 >= 80 && arp1 <= 100) {
         // DOWN
         led.plot(1, 2)
-        I2C_LCD1602.ShowString("D", 1, 0)
+        I2C_LCD1602.ShowString("dol", 1, 0)
         radio.sendString("D")
     } else if (arp1 >= 0 && arp1 <= 15) {
         // LEFT
         led.plot(0, 1)
-        I2C_LCD1602.ShowString("L", 0, 0)
+        I2C_LCD1602.ShowString("lewo", 0, 0)
         radio.sendString("L")
     } else if (arp1 >= 150 && arp1 <= 185) {
         // RIGHT
         led.plot(2, 1)
-        I2C_LCD1602.ShowString("R", 3, 0)
+        I2C_LCD1602.ShowString("prawo", 3, 0)
         radio.sendString("R")
     } else {
         // EMPTY
@@ -60,22 +58,22 @@ basic.forever(function () {
     } else if (arp2 >= 20 && arp2 <= 50) {
         // UP
         led.plot(3, 2)
-        I2C_LCD1602.ShowString("U2", 1, 0)
+        I2C_LCD1602.ShowString("przod", 1, 0)
         radio.sendString("1")
     } else if (arp2 >= 80 && arp2 <= 100) {
         // DOWN
         led.plot(3, 4)
-        I2C_LCD1602.ShowString("D2", 1, 0)
+        I2C_LCD1602.ShowString("tyl", 1, 0)
         radio.sendString("3")
     } else if (arp2 >= 0 && arp2 <= 15) {
         // LEFT
         led.plot(2, 3)
-        I2C_LCD1602.ShowString("L2", 0, 0)
+        I2C_LCD1602.ShowString("szczeka", 0, 0)
         radio.sendString("2")
     } else if (arp2 >= 150 && arp2 <= 185) {
         // RIGHT
         led.plot(4, 3)
-        I2C_LCD1602.ShowString("R2", 3, 0)
+        I2C_LCD1602.ShowString("szczeka", 3, 0)
         radio.sendString("4")
     } else {
         // EMPTY
